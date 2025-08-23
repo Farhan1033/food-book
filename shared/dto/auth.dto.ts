6132e4a385b6cd285810d47b3ceb28ba733ac192
Farhan1033 <hanzoi1006@gmail.com>
2025-08-23 15:13:01 +0700

    feat: update dto authentication

--- shared/dto/auth.dto.ts
+++ shared/dto/auth.dto.ts
@@ -2,9 +2,18 @@ export interface CreateUserRequest {
     fullName: string;
     email: string;
     password: string;
+    avatar: string | null;
+    bio: string | null;
 }
 
 export interface LoginUserRequest {
     email: string;
     password: string;
+}
+
+export interface UpdateUserRequest {
+    fullName?: string;
+    email?: string;
+    avatar?: string | null;
+    bio?: string | null;
 }
