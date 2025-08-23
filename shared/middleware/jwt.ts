a0dcc1ac85825d1018235ff3750dfa0c5ee540ad
Farhan1033 <hanzoi1006@gmail.com>
2025-08-23 15:57:59 +0700

    middleware updated with redis added

--- shared/middleware/jwt.ts
+++ shared/middleware/jwt.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import redis from "../../src/infrastructure/redisClient";
 
 export const SECRET_KEY: Secret = 'K0p1Luw4k';
 
 export interface CustomRequest extends Request {
     token: string | JwtPayload;
+    userId: string;
 }
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
@@ -17,6 +19,12 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
 
         const decoded = jwt.verify(token, SECRET_KEY);
         (req as CustomRequest).token = decoded;
+
+        const userId = await redis.get(token);
+        if (!userId) {
+            return res.status(401).json({ message: 'Invalid or expired session' });
+        }
+        (req as CustomRequest).userId = userId
 
         next();
     } catch (err) {
