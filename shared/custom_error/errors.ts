62f9ada847adb7ee7113441ef2c9c23ab0d091f2
Farhan1033 <hanzoi1006@gmail.com>
2025-08-23 15:12:12 +0700

    feat: modify custom error handling

--- shared/custom_error/errors.ts
+++ shared/custom_error/errors.ts
@@ -1,10 +1,3 @@
-const ErrInternalServerError = new Error("Internal Server Error")
-const ErrEmailFound = new Error("Email already registered")
-const ErrEmailNotFound = new Error("Email not found!")
-const ErrInputNull = new Error("Input cannot be empty")
-const ErrInvalidInput = new Error("Invalid input")
-const ErrPasswordInvalid = new Error("Incorrect password, please check again")
-
 export class CustomError extends Error {
     statusCode: number;
 
