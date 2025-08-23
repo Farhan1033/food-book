347d7820647e085aac7be67c9256574d4a012b0b
Farhan1033 <hanzoi1006@gmail.com>
2025-08-23 15:59:55 +0700

    adding logout to the router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { UserController } from "../handler/userController";
+import { auth } from "../../shared/middleware/jwt";
 
 const router = express.Router()
 
 //Auth router
 router.post('/register', UserController.signUp)
 router.post('/login', UserController.signIn)
+router.post("/logout", auth, UserController.logout);
 
 export default router;
